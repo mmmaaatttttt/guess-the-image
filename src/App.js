e85640262cd139d02c9f8624dc4de3ab9c08675b
Matt Lane <lane.matthew@gmail.com>
2021-07-21 11:19:30 -0700

    show start button first

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import { useState } from "react";
 import images from "./data/images";
 import { shuffle } from "./util/random";
-import ImageContainer from "./ImageContainer"
+import ImageContainer from "./ImageContainer";
 
 const shuffledImages = shuffle(images);
 
 function App() {
   const [started, setStarted] = useState(false);
   const [imageIdx, setImageIdx] = useState(0);
+  const nextImage = () => setImageIdx(oldIdx => oldIdx + 1);
 
-  return (
+  return started ? (
     <div>
-      <ImageContainer {...shuffledImages[imageIdx]} />
+      <ImageContainer {...shuffledImages[imageIdx]} onNext={nextImage} />
       <h3>{shuffledImages[imageIdx].title}</h3>
     </div>
+  ) : (
+    <button onClick={() => setStarted(true)}>Start Game</button>
   );
 }
 
