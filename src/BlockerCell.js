4ecd0efdf856d7e924937cdc9b6c3dd82c144f70
Matt Lane <lane.matthew@gmail.com>
2021-07-22 15:53:11 -0700

    remove BlockerCell.css

--- src/BlockerCell.js
+++ src/BlockerCell.js
@@ -1,9 +1,14 @@
 import { memo } from "react";
-import "./BlockerCell.css";
+import { Box } from "@chakra-ui/react";
 
 function BlockerCell({ width, height, offsetX, offsetY, hidden }) {
   return (
-    <div
+    <Box
+      opacity={hidden ? 0 : 1}
+      bg="aqua"
+      border="1px solid aqua"
+      position="absolute"
+      transition="opacity 1s"
       className={`BlockerCell ${hidden ? "hidden" : ""}`}
       style={{
         width: `${width}px`,
