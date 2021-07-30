de842c1d13795f7d351b8aa88d4c37c5b7bc6117
Matt Lane <lane.matthew@gmail.com>
2021-07-29 22:11:07 -0700

    export both data sets

---
+++ src/data/index.js
@@ -0,0 +1,15 @@
+import movieData from "./movies";
+import tvShowData from "./tvshows";
+
+const data = [
+  {
+    label: "Movie",
+    images: movieData
+  },
+  {
+    label: "TV Show",
+    images: tvShowData
+  }
+];
+
+export default data;
